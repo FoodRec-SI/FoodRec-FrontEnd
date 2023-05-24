351f114642bde963c09c4816a6b7307e6912d3bd
adiosNTV <nguyenthienvu12102003@gmail.com>
2023-05-24 21:56:13 +0700

    feat:add-recipe-card

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return{
+        <div className="banner">
+            <div className="banner__info">
+                <h1>Recipe Cart</h1>
+                <p>Find your favorite recipe</p>
+            </div>
+        </div>
+    }
+}
