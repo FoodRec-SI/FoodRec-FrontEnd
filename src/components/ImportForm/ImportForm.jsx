23aac417056b5fc76c70023beaa8092dfa61f958
adiosNTV <nguyenthienvu12102003@gmail.com>
2023-06-04 17:32:19 +0700

    feat:import-form

--- src/components/ImportForm/ImportForm.jsx
+++ src/components/ImportForm/ImportForm.jsx
@@ -1,15 +1,69 @@
-import { Button } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { TextField } from '@mui/material';
 import WallpaperIcon from '@mui/icons-material/Wallpaper';
 import Stack from '@mui/material/Stack';
 import FolderIcon from '@mui/icons-material/Folder';
+import ImageIcon from '@mui/icons-material/Image';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+
+import { useState } from 'react';
 
 import './ImportForm.css'
 
 
 const ImportForm = () => {
+
+    const [open, setOpen] = useState(false);
+    const [previewImg, setPreviewImg] = useState(null);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        let file = e.dataTransfer.files[0];
+        handleDropImage(file);
+    };
+
+
+    const handleDropImage = (file) => {
+
+        let validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/svg+xml'];
+        if (validExtensions.includes(file.type)) {
+            let fileReader = new FileReader();
+            fileReader.onload = () => {
+                let fileURL = fileReader.result;
+                setPreviewImg(fileURL);
+            };
+            fileReader.readAsDataURL(file);
+        } else {
+            alert('This is not an Image File!');
+        }
+    }
+
+    const handleBrowseOnClick = () => {
+        document.querySelector('input[type="file"]').click();
+    }
+
+    const handleInputFileChange = (e) => {
+        e.preventDefault();
+        let file = e.target.files[0];
+        handleDropImage(file);
+    }
+
+
+
     return (
         <div className="import-form">
             <div className="import-form__title">
@@ -41,14 +95,16 @@ const ImportForm = () => {
                 <div className="import-form__Detail__RecipeTitle">
                     <h2>Recipe Title</h2>
                     <p>250 characters max</p>
+                    
                     <TextField label="Enter Your Recipe Title"
                         fullWidth
                         variant="outlined"
                         size="larger" />
                 </div>
+                <br />
                 <div className="import-form__Detail__RecipeDescription">
                     <h2>Recipe Description</h2>
-                    <br />
+                    
                     <TextField
                         label="Enter Your Recipe Description"
                         multiline
@@ -57,27 +113,50 @@ const ImportForm = () => {
 
                     />
                 </div>
+                <br />
                 <div className="import-form__Detail__RecipeImage">
                     <h2>Recipe Image</h2>
-                    <div className="import-form__Detail__RecipeImage__upload">
+                    
+                    {/* <div className="import-form__Detail__RecipeImage__upload">
                         <WallpaperIcon fontSize='large'></WallpaperIcon>
                         <p> Uploading a picture to illustrate your recipe
-                            SVG, JPG, PNG ,... resolustion 700 x430px
+                            SVG, JPG, PNG ,... resolustion 1920x1080px
                         </p>
                         <Button variant="outlined"
-                        startIcon={<FolderIcon />}
-                        size='large'
-                        sx={{ borderRadius: '20px' }}                    
-                    >
-                        Browse
-                    </Button>
+                            onClick={handleClickOpen}
+                            startIcon={<FolderIcon />}
+                            size='large'
+                            sx={{ borderRadius: '20px' }}
+                        >
+                            Browse
+                        </Button>
+                    </div> */}
+                    <div className="import-form__Detail__RecipeImage__upload__preview">
+                        <h2>Upload Your Image</h2>
+                        <div className="import-form__Detail__RecipeImage__upload__preview__dragArea" onDragOver={handleDragOver} onDrop={handleDrop}>
+                            {/* <p>Drag and drop your image here</p> */}
+
+                            {!previewImg && <ImageIcon fontSize='large' />}
+                            {!previewImg && <span> Darg and Drop</span>}
+                            {!previewImg && <span> or</span>}
+                            
+                            {!previewImg && <Button onClick={handleBrowseOnClick} variant="text">Browes</Button>}
+                            {!previewImg && <input type='file' hidden onChange={handleInputFileChange}/>}
+                        
+                            {previewImg && <img src={previewImg} alt="" />}
+                            {previewImg && <div className="import-form__Detail__RecipeImage__upload__preview__dragArea__clearImg">
+                            <IconButton aria-label="delete" size="large" color='error' onClick={() => setPreviewImg(null)}>
+                                <CancelIcon />
+                            </IconButton>
+                        </div>}
+                        </div>
                     </div>
                 </div>
 
             </div>
 
 
-        </div>
+        </div >
     )
 }
 
