67eeabe5559f4af57ae8339fdb3aa55dd498206d
adiosNTV <nguyenthienvu12102003@gmail.com>
2023-06-09 14:39:50 +0700

    feat: make web responsive

--- src/components/RecipeCardList/RecipeCardList.jsx
+++ src/components/RecipeCardList/RecipeCardList.jsx
@@ -2,9 +2,9 @@ import "./RecipeCardList.css";
 
 import RecipeCard from "../RecipeCard/RecipeCard";
 
-const RecipeCardList = ({props,style,pending}) => {
+const RecipeCardList = ({props,pending}) => {
   return (
-    <div className="recipe-list" style={style && { flexWrap: 'nowrap' }} pending={pending}>
+    <div className="recipe-list" pending={pending}>
       {props.map((item) => (   
         <RecipeCard
           key={item.id}
